a390cc875c7d632de47250eec5d9586f43885cc4
Jesus Santos <jesussantosd877@gmail.com>
2020-12-21 14:20:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+paises = {"Colombia":["Bogota","Poblacion 503,M","PIB 52,504"],"Nicaraguas":["Managua","Poblacion 6,465M","PIB 13,12M"],"Ecuador":["Quito","Poblacion 17,08M","PIB 108,4M"],"Peru":["lima","Poblacion 31,99M","PIB 222M"]}
+print(paises)
+producto = {"Poroto 1.78","Arroz 2.90","Aceite 1.86","Leche 1.57","Salchicha 1.97","Jugo 1.45"}
+print(producto)
